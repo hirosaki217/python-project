c233a357d6c93a4a6abea63f03b20ce861b369db
hieu1924 <43205454+hirosaki217@users.noreply.github.com>
2024-05-16 17:14:08 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,7 +1,19 @@
 from http.server import BaseHTTPRequestHandler
 
 class handler(BaseHTTPRequestHandler):
-
+    import undetected_chromedriver as uc
+    url = 'https://api.myip.com/'
+    my_user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36"
+  
+    options = uc.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument(f"user-agent={my_user_agent}")
+  
+    # Initialize Chrome WebDriver with the specified options
+    driver = uc.Chrome(options=options)
+    driver.get(url)
+    page_source = driver.page_source
+    driver.quit()
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
